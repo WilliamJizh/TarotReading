4c14c2209e788617267de327c3c31dbe7a73446f
WilliamJizh <65744833+WilliamJizh@users.noreply.github.com>
2023-09-07 11:12:50 -0700

    update url

--- frontend/src/component/TarotReading.js
+++ frontend/src/component/TarotReading.js
@@ -23,7 +23,7 @@ const TarotReading = () => {
     const getReading = async () => {
         const response = await axios.get('/api/tarot', {
             params: { question },
-            baseURL: ' https://pacific-cove-28177.herokuapp.com/',
+            baseURL: 'https://web-production-f202.up.railway.app/',
         });
         setResult(response.data);
     };
